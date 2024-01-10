803d4ee459b2c2c67210c4db4c2be23f94acc7a9
molnard1 <101341428+molnard1@users.noreply.github.com>
2024-01-10 11:24:48 +0100

    title

--- src/js/components/ListPizzaPage.jsx
+++ src/js/components/ListPizzaPage.jsx
@@ -8,7 +8,7 @@ export default function ListPizzaPage() {
     const items = data.map((item) => (
         <Col key={item.id} sm={4} style={{ marginTop: '10px', marginBottom: '20px' }}>
             <Card>
-                <Card.Img variant="top" src={item.kepURL ?? "https://images.placeholders.dev?width=400&height=400"} onError={(e) => {
+                <Card.Img variant="top" alt={item.name} title={item.name} src={item.kepURL ?? "https://images.placeholders.dev?width=400&height=400"} onError={(e) => {
                     e.target.src = "https://images.placeholders.dev?width=400&height=400";
                 }} style={{ objectFit: 'cover', height: '25vw', width: '100%' }} />
                 <Card.Body style={{ textAlign: 'center' }}>
